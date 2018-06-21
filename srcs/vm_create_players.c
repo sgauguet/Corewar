22024f210cf6d3af803f9aa17855efda5156201f
Jean BOSSUET-TIERCELIN <jebossue@e3r8p18.42.fr>
2018-06-21 17:41:25 +0200

    need finish to check prog size

--- srcs/vm_create_players.c
+++ srcs/vm_create_players.c
@@ -6,7 +6,7 @@
 /*   By: sgauguet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/18 09:48:30 by sgauguet          #+#    #+#             */
-/*   Updated: 2018/06/21 12:26:11 by sgauguet         ###   ########.fr       */
+/*   Updated: 2018/06/21 17:39:55 by jebossue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,11 @@ int	ft_header_player(char *buf, t_env *env)
 	ft_memcpy(env->champions[env->nb_players].header.prog_name, &buf[sizeof(((header_t*)0)->magic)], PROG_NAME_LENGTH);
 	mod_padding = (sizeof(((header_t*)0)->prog_name) % 4) ? (4 - (sizeof(((header_t*)0)->prog_name) % 4)) : 0;
 	ft_memcpy(prog_size, &buf[sizeof(((header_t*)0)->magic) + sizeof(((header_t*)0)->prog_name) + mod_padding], 4);
+	if (prog_size[1] + prog_size[2] + prog_size[3] != //retour de read - header)
+		display_errors("prog_size != size instructions");
+	printf("decalage prog_size : %d\n", prog_size[1] << 16 & 0xff0000);
+	printf("decalage prog_size : %d\n", prog_size[2] << 8 & 0x00ff00);
+	printf("prog_size : %d\n", prog_size[3] & 0x0000ff);
 	ft_memcpy(env->champions[env->nb_players].header.comment, &buf[sizeof(((header_t*)0)->magic) + sizeof(((header_t*)0)->prog_name) + sizeof(((header_t*)0)->prog_size) + mod_padding], COMMENT_LENGTH);
 	return (1);
 }
