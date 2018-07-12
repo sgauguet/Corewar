bf2c0ff42c2ed0da371dd9842ade317c7737fabb
Jean BOSSUET-TIERCELIN <jebossue@e3r7p11.42.fr>
2018-07-12 14:58:55 +0200

    sub + add

--- includes/corewar.h
+++ includes/corewar.h
@@ -6,7 +6,7 @@
 /*   By: sgauguet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/12 16:53:01 by sgauguet          #+#    #+#             */
-/*   Updated: 2018/07/10 09:38:12 by sgauguet         ###   ########.fr       */
+/*   Updated: 2018/07/05 12:01:11 by sgauguet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,6 @@ typedef struct	s_player
 
 typedef struct	s_process
 {
-	unsigned long		id;
 	int					reg[REG_NUMBER];
 	int					current;
 	int					pc;
@@ -58,7 +57,6 @@ typedef struct	s_stack
 {
 	t_process	*first_process;
 	int			nb_process;
-	int			process_id;
 }				t_stack;
 
 typedef	struct	s_fork
@@ -67,14 +65,6 @@ typedef	struct	s_fork
 	int			carry;
 	int			alive;
 }				t_fork;
-
-typedef struct	s_param
-{
-	int			value[3];
-	int			size[3];
-	int			adress;
-	int			success;
-}				t_param;
 
 typedef struct	s_env 
 {
@@ -134,7 +124,6 @@ int				run_the_game(t_env *env);
 ** vm_destroy_process.c
 */
 
-int				free_memory(t_env *env, t_process *process);
 int				destroy_process(t_env *env, t_process *process);
 int				search_dead_process(t_env *env);
 
@@ -155,19 +144,13 @@ int				nb_cycles_instruction(t_env *env, t_process *process);
 int				size_instruction(t_env *env, t_process *process);
 
 /*
-** vm_exec_functions.c
-*/
-
-int				check_adress(int adress);
-int				params_size_ocp(t_env *env, t_process *process, t_param *param);
-int				copy_register(t_process *process, char *buf, int reg_number);
-void			modify_memory_content(t_env *env, char *buf, int start, int size);
-void			copy_memory_area(t_env *env, char *buf, int start, int size);
-
-/*
 ** vm_exec_instructions.c
 */
 
+int				check_adress(int adress);
+int				copy_register(t_process *process, char *buf, int reg_number);
+void			modify_memory_content(t_env *env, char *buf, int start, int size);
+void			copy_memory_area(t_env *env, char *buf, int start, int size);
 int				exec_instruction(t_env *env, t_process *process);
 
 /*
@@ -182,7 +165,6 @@ int				display_arena(t_env *env);
 ** vm_display_messages.c
 */
 
-int				show_operations(t_env *env, t_process *process, t_param *param);
 int				show_deaths(t_env *env, t_process *process);
 int				show_pc_movements(t_env *env, t_process *process);
 int				display_end(t_env *env);
@@ -215,5 +197,7 @@ int				exec_sti(t_env *env, t_process *process);
 int				exec_fork(t_env *env, t_process *process);
 int				exec_lfork(t_env *env, t_process *process);
 int				exec_aff(t_env *env, t_process *process);
+int				exec_add(t_env *env, t_process *process);
+int				exec_sub(t_env *env, t_process *process);
 
 #endif
