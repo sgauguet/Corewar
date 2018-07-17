ccd32495360b4d37da7845e3df6193334111d1a0
Jean BOSSUET-TIERCELIN <jebossue@e3r8p16.42.fr>
2018-07-17 15:47:21 +0200

    and

--- srcs/exec_and.c
+++ srcs/exec_and.c
@@ -6,13 +6,13 @@
 /*   By: jebossue <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/12 16:33:19 by jebossue          #+#    #+#             */
-/*   Updated: 2018/07/13 15:22:49 by jebossue         ###   ########.fr       */
+/*   Updated: 2018/07/17 15:43:20 by jebossue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
 
-int	check_and(t_env *env, t_process *process, t_param *param)
+int	check_and(t_env *env, t_process *process, t_param *param, char *reg)
 {
 	if ((process->ocp[0] != 1 && process->ocp[0] != 2 && process->ocp[0] != 3)
 		|| (process->ocp[1] != 1 && process->ocp[1] != 2 && process->ocp[1] != 3)
@@ -22,7 +22,7 @@ int	check_and(t_env *env, t_process *process, t_param *param)
 		return (0);
 	param->value[2] = (int)env->arena[check_adress(process->current + 2
 			+ param->size[0] + param->size[1])];
-	if (param->value[2] < 1 || param->value[2] > REG_NUMBER)
+	if (!copy_register(process, reg, param->value[2]))
 		return (0);
 	return (1);
 }
@@ -30,19 +30,23 @@ int	check_and(t_env *env, t_process *process, t_param *param)
 int	exec_and(t_env *env, t_process *process)
 {
 	t_param	param;
+	char	reg_value[4];
 	char	tmp[4];
 
-	if (!check_and(env, process, &param))
+	if (!check_and(env, process, &param, reg_value))
 		return (0);
-	copy_memory_area(env, tmp, check_adress(process->current + 1), param.size[0]);
-	param.value[0] = (param.size[0] == 2) ? (tmp[0] << 8 | (unsigned char)tmp[1]) : )
+	copy_memory_area(env, tmp, check_adress(process->current + 2), param.size[0]);
+	if (param.size[0] == 1 && ((int)tmp[0] < 1 || (int)tmp[0] > NB_INSTRUCTIONS))
+			return (0);
+	param.value[0] = (param.size[0] == 2) ? (tmp[0] << 8 | (unsigned char)tmp[1]) : register_value(process, (int)tmp[0]);
+	if (process->ocp[0] == 3)
+		param.value[0] = indirect_value(env, param.value[0]);
+	copy_memory_area(env, tmp, check_adress(process->current + 2 + param.size[0]), param.size[1]);
+	if (param.size[1] == 1 && ((int)tmp[0] < 1 || (int)tmp[0] > NB_INSTRUCTIONS))
+		return (0);
+	param.value[1] = (param.size[1] == 2) ? (tmp[0] << 8 | (unsigned char)tmp[1]) : register_value(process, (int)tmp[0]);
+	if (process->ocp[1] == 3)
+		param.value[1] = indirect_value(env, param.value[1]);
 
-
-/*	if (param.size[0] == 1)
-		param.value[0] = (int)env->arena[check_adress(process->current + 2)];
-	else if (param.size[0] == 2)
-	if (param.size[1] == 1)
-		param.value[1] = (int)env->arena[check_adress(process->current + 2
-				+ param.size[0])];*/
 	return (1);
 }
