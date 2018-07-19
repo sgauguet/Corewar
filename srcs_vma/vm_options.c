126a9625186902c16a2bd754d01f5f485eb6bf91
Simon GAUGUET <sgauguet@e3r8p15.42.fr>
2018-07-19 16:25:34 +0200

    option modifications

--- srcs_vma/vm_options.c
+++ srcs_vma/vm_options.c
@@ -6,47 +6,52 @@
 /*   By: sgauguet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/18 09:49:35 by sgauguet          #+#    #+#             */
-/*   Updated: 2018/07/19 15:44:03 by sgauguet         ###   ########.fr       */
+/*   Updated: 2018/07/19 16:23:53 by sgauguet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
 
-int		search_options(t_env *env, char **argv, int i)
+int		search_options(t_env *env, char **argv, int argc, int i)
 {
 	int ret;
+	int check;
 
 	ret = 0;
+	check = ((i + 1) < argc) ? 1 : 0;
 	if (ft_strlen(argv[i]) < 1 || argv[i][0] != '-')
 		return (0);
 	if (!ft_strcmp(argv[i], "-a") && (ret = 1))
 		env->option.a = 1;
-	else if (!ft_strcmp(argv[i], "-d") && (ret = 1))
+	else if (!ft_strcmp(argv[i], "-d") && check && (ret = 2))
 		env->option.d = ft_atoi(argv[i + 1]);
-	else if (!ft_strcmp(argv[i], "-s") && (ret = 1))
+	else if (!ft_strcmp(argv[i], "-s") && check && (ret = 2))
 		env->option.s = ft_atoi(argv[i + 1]);
-	else if (!ft_strcmp(argv[i], "-v") && (ret = 1))
+	else if (!ft_strcmp(argv[i], "-v") && check && (ret = 2))
 		env->option.v = ft_atoi(argv[i + 1]);
-	else if (!ft_strcmp(argv[i], "-b") && (ret = 1))
+	else if (!ft_strcmp(argv[i], "-b") && check && (ret = 2))
 		env->option.b = 1;
 	else if (!ft_strcmp(argv[i], "-n") && (ret = 1))
 		env->option.n = 1;
 	else if (!ft_strcmp(argv[i], "--stealth") && (ret = 1))
 		env->option.stealth = 1;
 	if (ret)
-		return (1);
+		return (ret);
 	return (0);
 }
 
-int		check_options(char **argv, t_env *env)
+int		check_options(char **argv, int argc, t_env *env)
 {
 	int	i;
+	int	ret;
 
 	i = 1;
 	while (argv[i])
 	{
-		if (!search_options(env, argv, i))
-			create_player(argv[i], env);		
+		if (!(ret = search_options(env, argv, argc, i)))
+			create_player(argv[i], env);
+		else if (ret == 2)
+			i++;
 		i++;
 	}
 	return (1);
