b0e9514c1bab78cd5509910f9507cbb27c38632c
Simon GAUGUET <sgauguet@e3r1p11.42.fr>
2018-07-20 16:53:22 +0200

    test du carry

--- vm_champs/champs/tests_instrutions/test_carry.s
+++ vm_champs/champs/tests_instrutions/test_carry.s
@@ -33,42 +33,103 @@
 	zjmp %3
 	or r3, r4, r6
 	zjmp %3
-	sub r3, r4, r8
-	zjmp %3
-	and r3, r4, r9
+	add r3, r4, r9
 	zjmp %3
 	
 ## carry -> 1
 	xor r3, r4, r7
 	zjmp %3
+
+## carry reste -> 1
+	sub r3, r4, r8
 	zjmp %3
-	and r1, r3, r4
+	and r10, r11, r12
 	zjmp %3
-	st r4, 1000
+	xor r10, r11, r12
+	zjmp %3
+	st r7, 100
+	zjmp %3
+	st r8, 100
+
+## carry -> 0
+
+	ld %1415681555534152, r6
 	zjmp %3
 
+## carry reste -> 0
+	add r4, r5, r10
+	zjmp %3
+	or r4, r5, r11
+	zjmp %3
+	xor r4, r15, r12
+	zjmp %3
 
-ld %1534152, r6
-zjmp %3
-and r3, r6, r7
-zjmp %3
-st r7, 1000
-zjmp %3
+## carry reste -> 0 ????? -> pas de modification du carry pour sti et ldi ?
+	ldi r2, r4, r2 
+	zjmp %3
+	live %-1
+	ldi %0, r4, r3 
+	zjmp %3
+	ldi %0, %0, r4 
+	zjmp %3
+	ldi %42, %-42, r5 
+	zjmp %3
+	ldi %4186415057412, %-1451541114710, r6 
+	zjmp %3
+	ldi -152, r4, r7 
+	zjmp %3
+	ldi -5663224125, r4, r8 
+	zjmp %3
+	sti r2, r4, r2 
+	zjmp %3
+	sti r3, r16, r3 
+	zjmp %3
+	sti r4, r4, %0
+	zjmp %3
+	sti r5, %42596, %-42596
+	zjmp %3
+	sti r6, %-365214832, r15
+	zjmp %3
+	sti r7, -1, %121212
+	zjmp %3
+	sti r8, 14, %-14
+	zjmp %3
 
+## carry -> 1
+	and r16, r4, r16
+	zjmp %3
 
-ld %-1515102, r8
-zjmp %3
-and r7, r8, r9
-zjmp %3
-st r9, 1000
-zjmp %3
+## carry reste -> 1
+	ldi r2, r4, r2 
+	zjmp %3
+	live %-1
+	ldi %0, r4, r3 
+	zjmp %3
+	ldi %0, %0, r4 
+	zjmp %3
+	ldi %42, %-42, r5 
+	zjmp %3
+	ldi %4186415057412, %-1451541114710, r6 
+	zjmp %3
+	ldi -152, r4, r7 
+	zjmp %3
+	ldi -5663224125, r4, r8 
+	zjmp %3
+	sti r2, r4, r2 
+	zjmp %3
+	sti r3, r16, r3 
+	zjmp %3
 
-sti r2, r3, r4
-zjmp %3
-sti r21, r3, r4
-zjmp %3
-sti r2, r32, r4
-zjmp %3
-sti r2, r3, r41
-zjmp %3
-lfork %-1513854841512
+## carry -> 0
+	lld %12, r14
+	zjmp %3
+
+## carry reste -> 0
+	lld %1864815, r14
+	zjmp %3
+	lld %-164183548, r14
+	zjmp %3
+	lld r3, r14
+	zjmp %3
+	lld %12, r14
+	zjmp %3
